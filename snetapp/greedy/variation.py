1981237bc82d52beeb5d51401043f56d66bfc3d8
Nuullll <vfirst218@gmail.com>
2019-05-20 17:16:13 +0800

    Re-evaluate variations.

---
+++ snetapp/greedy/variation.py
@@ -0,0 +1,61 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Filename: epochs
+# @Date: 2019-05-18-13-49
+# @Author: Nuullll (Yilong Guo)
+# @Email: vfirst218@gmail.com
+
+
+from snetapp.greedy.base import GreedyBaseWorker
+
+
+class TrainWithVariation(GreedyBaseWorker):
+
+    def __init__(self, options=None):
+        if options is None:
+            self.options = self.get_default_options()
+        else:
+            self.options = options
+
+        super(TrainWithVariation, self).__init__(self.options)
+
+    def get_default_options(self):
+        options = super(TrainWithVariation, self).get_default_options()
+
+        options.update({
+            'greedy': True,
+            'pattern_firing_rate': 1.,  # unit: spikes/dt
+            'background_firing_rate': 5.,
+            't_background_phase': 10,  # unit: dt
+            'training_samples': 10000,
+            'testing_samples': 1000,
+
+            'include_categories': [0, 1, 2],
+            't_training_image': 200,
+            'v_th_rest': 0.4,
+            'output_number': 12,
+        })
+
+        return options
+
+    def post_epoch(self, i, prefix=''):
+
+        if i % 1000 == 0:
+            self.save(filename=f"{prefix}{i}-worker.pickle")
+
+            self.network.W.plot_weight_map(out_file=self.get_path(f'{prefix}{i}-weights.png'))
+            self.network.W.plot_update_map(out_file=self.get_path(f'{prefix}{i}-updates.png'))
+
+            # self.network.INPUT.background_firing_rate -= rate_drop
+            # self.logger.info(f"Rate dropped to: {self.network.INPUT.background_firing_rate}")
+
+
+def train_baseline():
+    worker = TrainWithVariation()
+    worker.train(prefix='baseline-')
+    worker.test(worker.greedy_test)
+
+
+if __name__ == '__main__':
+    train_baseline()
